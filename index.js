a758cece31df43efbf53e77b196749b617a5b163
Ahmad8881 <1221mohammadahmad@gmail.com>
2024-10-18 15:45:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+console.log("hello")
+// question No. 1
+
+// let A = 2;
+// let B = 5;
+// let result = A + B;
+// console.log(result);
+
+// question No. 2
+// let A = 6;
+// let B = 3;
+
+    
+//     if (A < 10 && B < 10 && A > B) {
+//         console.log(true)
+//     } else {
+//          console.log(false)
+//     }
+
+
+
+// question No. 3
+
+
+// let A = 12;  
+// let B = 20;
+
+
+// if (A % 10 === 0 && B % 10 === 0) {
+//     console.log(true);
+// }
+
+// else if (A % 10 !== 0 && B % 10 !== 0) {
+//     console.log(false);
+// }
+
+// else {
+//     console.log(true);
+// }
+
+// que No.= 4
+
+// let a = 4567;
+// let res = a/1000;
+// console.log(Math.floor(res))
+
+// question No. 5
+
+// let N = 4567;  
+// console.log(N % 10);
+
+// que No.6
+
+// let a = 2
+// let b = 9
+// console.log(b % a)
+
+// que No.7
+
+// let a = 5
+// let b = 2
+// console.log(a*b)
+
+// que No.8
+
+let A = 50;  
+let B = 20;
+let C = 100;
+
+
+let total = A + B + C;
+
+let average = total / 3;
+
+
+console.log(total);      
+console.log(average)
